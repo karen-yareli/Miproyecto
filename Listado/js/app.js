8e0a0fd2a5d6a51541b8503ac15c8ddd4a17c987
Karen-yareli <iinf18.kcotoa@itesco.edu.mx>
2022-06-06 11:21:40 -0500

    actualizando listados ccs de pontada interna y pafinas 1 2

--- Listado/js/app.js
+++ Listado/js/app.js
@@ -1,39 +1,18 @@
-const arregloEstudiantes= [
-    new Estudiante('Pepporoni',   'Triplequeso'),
-    
+const arregloEstudiantes = [new Estudiante("Pepporoni", "Triplequeso")];
 
+function mostrarEstudiantes() {
+  let Texto = "";
+  for (let item of arregloEstudiantes) {
+    Texto += `<li>${item.nombre} ${item.apellido}</li>`;
+  }
+  document.getElementById("alumnos").innerHTML = Texto;
+}
 
-    ]
-    
-    function mostrarEstudiantes(){
-        let Texto =''
-        for(let item of arregloEstudiantes){
-        Texto += `<li>${item.nombre} ${item.apellido}</li>`
-        }
-        document.getElementById('alumnos').innerHTML = Texto 
-   
-       
-        
-        }
-
-
-        function agregarEstudiante(){
-            const formulario = document.forms["formulario"]
-            const nombre = formulario["nombre"]
-            const apellido = formulario["apellido"]
-            const objEstudiante = new Estudiante(nombre.value, apellido.value)
-            arregloEstudiantes.push(objEstudiante)
-            mostrarEstudiantes()
-           
-
-
-        }
-
-        
-        
-           
-        
-
-
-        
-   
+function agregarEstudiante() {
+  const formulario = document.forms["formulario"];
+  const nombre = formulario["nombre"];
+  const apellido = formulario["apellido"];
+  const objEstudiante = new Estudiante(nombre.value, apellido.value);
+  arregloEstudiantes.push(objEstudiante);
+  mostrarEstudiantes();
+}
